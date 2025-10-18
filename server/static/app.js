29ac41c693bdda005af051f69546600bccf65742
Febin Jacob Biju <febinjb777@gmail.com>
2025-10-18 21:04:29 +0530

    initial commit

---
+++ server/static/app.js
@@ -0,0 +1,65 @@
+function getBathValue() {
+  var uiBathrooms = document.getElementsByName("uiBathrooms");
+  for (let i = 0; i < uiBathrooms.length; i++) {
+    if (uiBathrooms[i].checked) {
+      return parseInt(uiBathrooms[i].value);
+    }
+  }
+  return -1; // Invalid Value
+}
+
+function getBHKValue() {
+  var uiBHK = document.getElementsByName("uiBHK");
+  for (let i = 0; i < uiBHK.length; i++) {
+    if (uiBHK[i].checked) {
+      return parseInt(uiBHK[i].value);
+    }
+  }
+  return -1; // Invalid Value
+}
+
+function onClickedEstimatePrice() {
+  console.log("Estimate price button clicked");
+  var sqft = document.getElementById("uiSqft").value;
+  var bhk = getBHKValue();
+  var bathrooms = getBathValue();
+  var location = document.getElementById("uiLocations").value;
+  var estPrice = document.getElementById("uiEstimatedPrice");
+
+  console.log("Sending data -> Sqft:", sqft, "BHK:", bhk, "Bath:", bathrooms, "Location:", location);
+
+  var url = "http://127.0.0.1:5000/predict_home_price";
+
+  $.post(url, {
+    total_sqft: parseFloat(sqft),
+    bhk: bhk,
+    bath: bathrooms,
+    location: location
+  }, function (data, status) {
+    console.log("Received response:", data);
+    estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
+  }).fail(function (xhr, status, error) {
+    console.error("Error:", error);
+    estPrice.innerHTML = "<h2>Error predicting price</h2>";
+  });
+}
+
+function onPageLoad() {
+  console.log("document loaded");
+  var url = "http://127.0.0.1:5000/get_location_names";
+
+  $.get(url, function (data, status) {
+    console.log("got response for get_location_names request");
+    if (data) {
+      var locations = data.locations;
+      var uiLocations = document.getElementById("uiLocations");
+      $('#uiLocations').empty();
+      for (var i in locations) {
+        var opt = new Option(locations[i]);
+        $('#uiLocations').append(opt);
+      }
+    }
+  });
+}
+
+window.onload = onPageLoad;
