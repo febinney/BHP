452256b9db0202665a24351dc4170904ac0a8372
Febin Jacob Biju <febinjb777@gmail.com>
2025-10-20 09:43:51 +0530

    Configure Flask app to run on dynamic port and host settings

--- server/server.py
+++ server/server.py
@@ -32,4 +32,6 @@ def predict_home_price():
 if __name__ == "__main__":
     print("Starting Python Flask Server for Home Price Prediction...")
     util.load_saved_artifacts()
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    
